7afd77bc99eace65af0fdfddd93a40b5f1757f9a
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-07-21 21:56:19 +0900

    Add Home

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { createBrowserHistory } from 'history';
 import * as React from 'react';
 /* import Alert from 'react-bootstrap/Alert';
  * import Nav from 'react-bootstrap/Nav'; */
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import { Route, Router } from "react-router-dom";
 import './App.css';
 import background1 from './background1.jpg';
 import About from './Components/About';
+import Home from './Components/Home';
 import Navigation from './Components/Navigation';
 
 /* import logo from './logo.svg'; */
@@ -20,19 +18,10 @@ class App extends React.Component {
             <Router history={history}>
                 <div className="App" data-background={background1}>
                     <Navigation />
-                    <section>
-                        <Container>
-                            <Row>
-                                <Col lg={6}>
-                                    <h1 className="mt-5">&nbsp;</h1>
-                                    <p>&nbsp;</p>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </section>
+                    <Route exact={true} path='/' component={Home} />
+                    <Route path='/about' component={About} />
                 </div>
-                <Route exact={true} path='/' component={About} />
-                <Route path='/about' component={About} />
+
                 {/*<Route path='/service' component={About} />
                     <Route path='/contact' component={About} /> */}
             </Router>
---
+++ src/Components/Home.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+class Home extends React.Component {
+    public render() {
+        return (
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg={12}>
+                            <h1 className="mt-5 text-right">E-Commerce Specialist</h1>
+                            <p>&nbsp;</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>);
+    }
+}
+export default Home;
