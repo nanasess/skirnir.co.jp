c6ebb68472d4a506ae8e3cfe3c6392b77f7ddb12
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-07-21 21:41:01 +0900

    Implement to Navgation

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import * as React from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import { Router } from "react-router-dom";
-/* import { Link, Route, Router } from "react-router-dom"; */
-/* import About from './About'; */
+import { Route, Router } from "react-router-dom";
 import './App.css';
 import background1 from './background1.jpg';
+import About from './Components/About';
+import Navigation from './Components/Navigation';
 
 /* import logo from './logo.svg'; */
 
@@ -19,34 +19,7 @@ class App extends React.Component {
         return (
             <Router history={history}>
                 <div className="App" data-background={background1}>
-                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
-                        <div className="container">
-                            <a className="navbar-brand" href="#">スキルニル株式会社</a>
-                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon" />
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarResponsive">
-                                <ul className="navbar-nav ml-auto">
-                                    <li className="nav-item active">
-                                        <a className="nav-link" href="#">Home
-                                            <span className="sr-only">(current)</span>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        {/* <Link to="/about">About</Link> */}
-                                    </li>
-                                    <li className="nav-item">
-                                        {/* <Link to="/service">Services</Link> */}
-                                        <a className="nav-link" href="#">Services</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        {/* <Link to="/contact">Contact</Link> */}
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-
+                    <Navigation />
                     <section>
                         <Container>
                             <Row>
@@ -58,9 +31,9 @@ class App extends React.Component {
                         </Container>
                     </section>
                 </div>
-                {/* <Route exact={true} path='/' component={About} />
-                    <Route path='/about' component={About} />
-                    <Route path='/service' component={About} />
+                <Route exact={true} path='/' component={About} />
+                <Route path='/about' component={About} />
+                {/*<Route path='/service' component={About} />
                     <Route path='/contact' component={About} /> */}
             </Router>
         );
---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from 'react-router-dom';
+
+class Navigation extends React.Component<any, {}> {
+    public render() {
+        const navbar: JSX.Element = (
+            <Navbar expand="lg" variant="dark" bg="dark" fixed="bottom">
+                <Container>
+                    <Navbar.Brand href="#">スキルニル株式会社</Navbar.Brand>
+                    <Navbar.Toggle type="button" area-controls="navbarResponsive">
+                        <span className="navbar-toggler-icon" />
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="navbarResponsive">
+                        <Nav className="ml-auto" as="ul">
+                            <Nav.Item as="li">
+                                <NavLink to="/" className="nav-link">
+                                    Home
+                                    <span className="sr-only">(current)</span>
+                                </NavLink>
+                            </Nav.Item>
+                            <Nav.Item as="li">
+                                <NavLink to="/about" className="nav-link">
+                                    About
+                                </NavLink>
+                            </Nav.Item>
+                            <Nav.Item as="li">
+                                <NavLink to="/services" className="nav-link">
+                                    Services
+                                </NavLink>
+                            </Nav.Item>
+                            <Nav.Item as="li">
+                                <NavLink to="/contact" className="nav-link">
+                                    Contact
+                                </NavLink>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+        return navbar;
+    }
+}
+
+export default Navigation;
