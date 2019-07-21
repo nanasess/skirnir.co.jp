36e56cb7f0e9d6680986ee514723612e18d4f0da
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-07-21 23:15:11 +0900

    Fix services

--- src/Components/Services.tsx
+++ src/Components/Services.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 class Services extends React.Component {
     public render() {
@@ -10,9 +11,12 @@ class Services extends React.Component {
                         <Col lg={12}>
                             <h1 className="mt-5 text-left">Services</h1>
                             <p>高度な技術力を活用して、E-Commerce を中心に技術開発をしています。</p>
-                            <p><a href="https://github.com/nanasess">Github のプロフィール</a></p>
-                            <p><a href="https://www.ec-cube.net/evangelist/">「EC-CUBE」開発コミッター・公式エヴァンジェリスト</a></p>
-                            <p><a href="https://www.slideshare.net/nanasess/emacs-5282932">GNU Emacs icons contributor</a></p>
+                            <ul>
+                                <li><a href="https://github.com/nanasess">Github のプロフィール</a></li>
+                                <li><a href="https://www.ec-cube.net/evangelist/">「EC-CUBE」開発コミッター・公式エヴァンジェリスト</a></li>
+                                <li><a href="https://www.slideshare.net/nanasess/emacs-5282932">GNU Emacs icons contributor</a></li>
+                                <li><Link to="/contact">ご依頼はお気軽に</Link></li>
+                            </ul>
                         </Col>
                     </Row>
                 </Container>
