e9ed0acc9613422f4ff0d4b2d97cb544c9f562d2
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-08-15 15:37:28 +0900

    Bump version to react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import About from './Components/About';
 import Contact from './Components/Contact';
@@ -7,23 +7,23 @@ import Home from './Components/Home';
 import Navigation from './Components/Navigation';
 import Services from './Components/Services';
 import TradeLaw from './Components/TradeLaw';
-import background1 from './Images/background1.jpg';
+/* import background1 from './Images/background1.jpg'; */
 
 /* import logo from './logo.svg'; */
 
 class App extends React.Component {
     public render() {
         return (
-            <Router>
-                <div className="App" data-background={background1}>
-                    <Navigation />
-                    <Route exact={true} path='/' component={Home} />
-                    <Route path='/about' component={About} />
-                    <Route path='/services' component={Services} />
-                    <Route path='/contact' component={Contact} />
-                    <Route path='/tradelaw' component={TradeLaw} />
-                </div>
-            </Router>
+            <HashRouter>
+                <Navigation />
+                <Routes>
+                    <Route index={false} path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/services' element={<Services />} />
+                    <Route path='/contact' element={<Contact />} />
+                    <Route path='/tradelaw' element={<TradeLaw />} />
+                </Routes>
+            </HashRouter>
         );
     }
 }
